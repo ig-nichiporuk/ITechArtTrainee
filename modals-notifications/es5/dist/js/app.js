4ad01ab6d7b38c652561b20bb5e703646798aabe
IgorNichiporuk <60503041+IgorNichiporuk@users.noreply.github.com>
2022-04-17 15:02:40 +0300

    change logic

--- modals-notifications/es5/dist/js/app.js
+++ modals-notifications/es5/dist/js/app.js
@@ -1,46 +1,14 @@
-var app = document.getElementById('app'),
-	notificationWrap = document.querySelector('.notificationsJS'),
-	modalWrap = document.querySelector('.modalJs'),
-
-	errorConf = {type: 'error', title: 'Error', desc: 'Something is wrong', icon: 'error-icon'},
-	successConf = {type: 'success', title: 'Success', desc: 'Everything is fine', icon: 'success-icon'},
-	warningConf = {type: 'warning', title: 'Warning', desc: 'You must be careful', icon: 'warning-icon'},
-	infoConf = {type: 'info', title: 'Info', desc: 'Useful information for you', icon: 'info-icon'},
+var	notificationConf = {
+		'error': {type: 'error', title: 'Error', desc: 'Something is wrong', icon: 'error-icon'},
+		'success': {type: 'success', title: 'Success', desc: 'Everything is fine', icon: 'success-icon'},
+		'warning': {type: 'warning', title: 'Warning', desc: 'You must be careful', icon: 'warning-icon'},
+		'info': {type: 'info', title: 'Info', desc: 'Useful information for you', icon: 'info-icon'},
+	},
 
 	modalConf = {type: 'info', title: 'Info', desc: 'Useful information for you', icon: 'info-icon'},
 
-	errorNotification = new Notification(errorConf, notificationWrap),
-	successNotification = new Notification(successConf, notificationWrap),
-	warningNotification = new Notification(warningConf, notificationWrap),
-	infoNotification = new Notification(infoConf, notificationWrap),
+	notification = new Notification(),
+	modal = new Modal();
 
-	modal = new Modal(modalConf, modalWrap);
-
-
-app.addEventListener('click', function (e) {
-	if (e.target.classList.contains('modalOpenJS')) {
-		event.preventDefault();
-
-		modal.show();
-	}
-
-	if (e.target.classList.contains('notificationOpenJS')) {
-		switch (e.target.dataset.type) {
-			case 'success':
-				successNotification.show();
-				break;
-			case 'warning':
-				warningNotification.show();
-				break;
-			case 'error':
-				errorNotification.show();
-				break;
-			case 'info':
-				infoNotification.show();
-				break;
-		}
-	}
-});
-
-Notification.prototype.setActions();
-Modal.prototype.setActions();
+notification.setActions(notificationConf, 5000);
+modal.setActions(modalConf);
